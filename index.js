52cf85eb0850c3e72205057b01eb9680edfce74c
Eyu-Gen <dreamersthebig@gmail.com>
2024-05-20 22:10:36 +0545

    Completed

--- index.js
+++ index.js
@@ -1,49 +1,3 @@
-// let buttons = document.getElementsByClassName("buttons");
-// let clickedButton = null;
-
-// for (let i = 0; i < buttons.length; i++) {    
-//     buttons[i].addEventListener("mouseover", function() {
-//         if (this !== clickedButton) {
-//             this.style.backgroundColor = "var(--primaryColor)";
-//             this.style.color = "var(--bodyColor)";
-//         }
-//     });
-
-//     buttons[i].addEventListener("mouseout", function() {
-//         if (this !== clickedButton) {
-//             this.style.backgroundColor = "var(--minorColor)";
-//             this.style.color = "white";
-//         }
-//     });
-    
-//     buttons[i].addEventListener("click", function() {
-//         for (let j = 0; j < buttons.length; j++) {
-//             buttons[j].style.backgroundColor = "var(--minorColor)";
-//             buttons[j].style.color = "white";
-//         }
-//         this.style.backgroundColor = "white";
-//         this.style.color = "var(--bodyColor)";
-        
-//         clickedButton = this;
-//     });
-// }
-
-// // Set up the submit button event listener outside the loop
-// let submit = document.getElementById("submitBtn");
-// let mainBlock = document.getElementById("mainBlock");
-// let secondaryBlock = document.getElementById("thankYouBlock");
-// let rate = document.getElementById("rate");
-
-// if (submit) {
-//     submit.addEventListener("click", function() {
-//         if (clickedButton != null) {
-//             mainBlock.style.display = "none";
-//             secondaryBlock.style.display = "flex";
-//             rate.innerHTML = clickedButton;
-//         }
-//     });
-// }
-
 let buttons = document.getElementsByClassName("buttons");
 let clickedButton = null;
 
@@ -74,7 +28,6 @@ for (let i = 0; i < buttons.length; i++) {
     });
 }
 
-// Set up the submit button event listener outside the loop
 let submit = document.getElementById("submitBtn");
 let mainBlock = document.getElementById("mainBlock");
 let secondaryBlock = document.getElementById("thankYouBlock");
